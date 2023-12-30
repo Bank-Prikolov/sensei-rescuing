2dec2951de683f96166f8f72a9dcd0350d0e9189
WaizorSote <orlan6128@gmail.com>
2023-12-30 18:07:42 +0300

    "отсортировал"

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import sys
 import screeninfo
+from Load_image import load_image
 
 pygame.init()
 size = width, height = 1024, 704
@@ -13,22 +14,6 @@ otstupy = (fullsize[1] - size[1] * k) * k
 screen = pygame.display.set_mode(size)
 last_screen = pygame.display.Info().current_w, pygame.display.Info().current_h
 test = 'pp_test_level.txt'
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
 
 
 class Hero(pygame.sprite.Sprite):
