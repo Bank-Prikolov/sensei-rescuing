8e076c150072d0e4f71c179ad775f266baf54169
Alexandr <mikhalchenkov22@gmail.com>
2023-12-31 12:58:19 +0300

    наработки в menu.py

--- menu.py
+++ menu.py
@@ -1,10 +1,9 @@
 import pygame, sys, screeninfo
 from load_image import *
+from buttons import Button
 
-size = WIDTH, HEIGHT = 800, 400
-clock = pygame.time.Clock()
-FPS = 50
-screen = pygame.display.set_mode(size)
+pygame.init()
+
 
 def terminate():
     pygame.quit()
@@ -12,30 +11,31 @@ def terminate():
 
 
 def start_menu():
-    intro_text = ["SENSEI RESCUING",
-                  "СТАРТ",
-                  "НАСТРОЙКИ",
-                  "ОБ АВТОРАХ"]
-
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
+    size = WIDTH, HEIGHT = 800, 400
+    clock = pygame.time.Clock()
+    FPS = 60
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Menu')
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+                sys.exit()
         pygame.display.flip()
         clock.tick(FPS)
+
+    running = True
+    fps = 60
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()
+
+
+start_menu()
+
