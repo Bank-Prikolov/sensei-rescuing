dbc352cd8238b91eaa79751e987cd5d540e89ad2
Alexandr <mikhalchenkov22@gmail.com>
2024-01-01 14:29:18 +0300

    наработки меню игры

--- menu.py
+++ menu.py
@@ -1,29 +1,30 @@
 import pygame, sys, screeninfo
+from PIL import Image
 from load_image import *
 from buttons import Button
 from const import *
 pygame.init()
 
-size = WIDTH, HEIGHT = 600, 550
+size = WIDTH, HEIGHT = 640, 480
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 pygame.display.set_caption('Menu')
 
-start_btn = Button(WIDTH / 2 - (252 / 2), 100, 252, 74, "", "start-btn.png", "hover-start-btn.png")
+# start_btn = Button(WIDTH / 2 - (252 / 2), 100, 252, 74, "", "start-btn.png", "hover-start-btn.png")
 
 
 def start_menu():
     running = True
     fps = 60
-    screen.blit(load_image('background1.png'), (0, 0))
+    screen.blit(load_image('menu-bg.png'), (0, 0))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         clock.tick(fps)
 
-        start_btn.check_hover(pygame.mouse.get_pos())
-        start_btn.draw(screen)
+        #start_btn.check_hover(pygame.mouse.get_pos())
+        #start_btn.draw(screen)
         pygame.display.flip()
 
 
