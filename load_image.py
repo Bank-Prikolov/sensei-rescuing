4edd2203a93b93d787bc7571f4785e05ac508abe
Alexandr <mikhalchenkov22@gmail.com>
2024-01-01 12:14:04 +0300

    наработки меню игры

--- load_image.py
+++ load_image.py
@@ -4,7 +4,7 @@ import os
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join('data', 'buttons', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
--- menu.py
+++ menu.py
@@ -1,40 +1,30 @@
 import pygame, sys, screeninfo
 from load_image import *
 from buttons import Button
-
+from const import *
 pygame.init()
 
+size = WIDTH, HEIGHT = 600, 550
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+pygame.display.set_caption('Menu')
 
-def terminate():
-    pygame.quit()
-    sys.exit()
+start_btn = Button(WIDTH / 2 - (252 / 2), 100, 252, 74, "", "start-btn.png", "hover-start-btn.png")
 
 
 def start_menu():
-    size = WIDTH, HEIGHT = 800, 400
-    clock = pygame.time.Clock()
-    FPS = 60
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Menu')
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-                sys.exit()
-        pygame.display.flip()
-        clock.tick(FPS)
-
     running = True
     fps = 60
+    screen.blit(load_image('background1.png'), (0, 0))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill((0, 0, 0))
         clock.tick(fps)
+
+        start_btn.check_hover(pygame.mouse.get_pos())
+        start_btn.draw(screen)
         pygame.display.flip()
-    pygame.quit()
 
 
 start_menu()
