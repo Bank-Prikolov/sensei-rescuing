9f3195c7270a75b8aaf143eb5b1359d90855267f
Alexandr <mikhalchenkov22@gmail.com>
2024-01-07 01:46:44 +0300

    наработки в menu.py

--- menu.py
+++ menu.py
@@ -2,40 +2,57 @@ import pygame, sys, screeninfo
 from load_image import *
 from buttons import Button
 from const import *
+
 pygame.init()
 
 size = WIDTH, HEIGHT = 1024, 704
 screen = pygame.display.set_mode(size)
-clock = pygame.time.Clock()
 pygame.display.set_caption('Sensei Rescuing')
-
-pygame.mixer.music.load("data\sounds\menu-sound.mp3")
-pygame.mixer.music.play(-1)
-
-start_btn = Button(WIDTH - 312, 100, 260, 100, "buttons\start-btn.png", "buttons\hover-start-btn.png",
-                   "data\sounds\menu-button-sound.mp3")
-
-tmp = load_image('buttons\pp_menu_bg.png')
-tmp1 = pygame.transform.scale(tmp, (tmp.get_width() * 2, tmp.get_height() * 2))
+clock = pygame.time.Clock()
 
 
-def start_menu():
+# pygame.mixer.music.load("data\sounds\menu-sound.mp3")
+# pygame.mixer.music.play(-1)
+
+
+def main_menu():
+    img = load_image('buttons\pp_menu_bg.png')
+    bg = pygame.transform.scale(img, (img.get_width() * 2, img.get_height() * 2))
+
+    start_btn = Button(WIDTH - 265, 168, 190, 80, "buttons\start-btn.png", "buttons\hover-start-btn.png",
+                       "data\sounds\menu-button-sound.mp3")
+    settings_btn = Button(WIDTH - 265, 248, 190, 80, "buttons\settings-btn.png", "buttons\hover-settings-btn.png",
+                          "data\sounds\menu-button-sound.mp3")
+    info_btn = Button(WIDTH - 265, 328, 190, 80, "buttons\info-btn.png", "buttons\hover-info-btn.png",
+                      "data\sounds\menu-button-sound.mp3")
+    exit_btn = Button(WIDTH - 265, 408, 190, 80, "buttons\exit-btn.png", "buttons\hover-exit-btn.png",
+                      "data\sounds\menu-button-sound.mp3")
+
+    buttons = [start_btn, settings_btn, info_btn, exit_btn]
+
     running = True
-    fps = 60
-    screen.blit(tmp1, (0, 0))
+    screen.blit(bg, (0, 0))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-            start_btn.handle_event(event)
+            for button in buttons:
+                button.handle_event(event)
 
-        clock.tick(fps)
+        for button in buttons:
+            button.check_hover(pygame.mouse.get_pos())
+            button.draw(screen)
 
-        start_btn.check_hover(pygame.mouse.get_pos())
-        start_btn.draw(screen)
         pygame.display.flip()
 
 
-start_menu()
+def settings_menu():
+    pass
 
+
+def about_authors_menu():
+    pass
+
+
+main_menu()
