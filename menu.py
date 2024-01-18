debccff047fd86e4b25f93b063886e9a3479e013
Alexandr <mikhalchenkov22@gmail.com>
2024-01-18 16:11:34 +0300

    наработки в menu

--- menu.py
+++ menu.py
@@ -3,11 +3,7 @@ import sys
 import webbrowser
 from load_image import load_image
 from itemCreator import Object, Button, Stars
-from transition import transition
-from consts import *
-from windows import fullscreen, k
-
-# сначала в меню, инфо и сеттингс пересчитать x и Y относительно title, потом сделать fs
+from windows import fullscreen
 
 pygame.init()
 
@@ -25,17 +21,18 @@ bg = pygame.transform.scale(img, (img.get_width() * 2, img.get_height() * 2))
 
 checkIsActive2 = False
 checkIsActiveBoss = False
-checkF11 = False
 isSliderMusic = False
 isSliderSound = False
 
-checkIsPassing1 = True
-checkIsPassing2 = False
-checkIsPassingBoss = False
+checkF11 = fullscreen
 
-record1 = 0
-record2 = 0
-record3 = 0
+checkIsPassing1 = True
+checkIsPassing2 = True
+checkIsPassingBoss = True
+
+record1 = 1
+record2 = 2
+record3 = 3
 
 a = open(r"data\settings.txt", "r", encoding="utf-8")
 checkActDet = list(map(lambda x: float(x.rstrip('\n')), a))
@@ -54,18 +51,23 @@ def main_menu():
         ft = False
     pygame.mixer.music.set_volume(wM)
 
-    title = Object(WIDTH // 2 - 886 // 2, 49, 886, 80, r"objects\menu-title-obj.png")
+    if not checkF11:
+        all_w, all_h = WIDTH // 2 - 443, HEIGHT - 655
+    else:
+        all_w, all_h = WIDTH // 2 - 443, HEIGHT - 820
 
-    start_btn = Button(WIDTH // 2 - 240 // 2, 186, 240, 100, r"buttons\default-start-btn.png",
+    title = Object(all_w, all_h, 886, 80, r"objects\menu-title-obj.png")
+
+    start_btn = Button(all_w + 323, all_h + 129, 240, 100, r"buttons\default-start-btn.png",
                        r"buttons\hover-start-btn.png", r"buttons\press-start-btn.png",
                        r"data\sounds\menu-button-sound.mp3")
-    settings_btn = Button(WIDTH // 2 - 240 // 2, 284, 240, 100, r"buttons\default-settings-btn.png",
+    settings_btn = Button(all_w + 323, all_h + 229, 240, 100, r"buttons\default-settings-btn.png",
                           r"buttons\hover-settings-btn.png", r"buttons\press-settings-btn.png",
                           r"data\sounds\menu-button-sound.mp3")
-    info_btn = Button(WIDTH // 2 - 240 // 2, 382, 240, 100, r"buttons\default-info-btn.png",
+    info_btn = Button(all_w + 323, all_h + 329, 240, 100, r"buttons\default-info-btn.png",
                       r"buttons\hover-info-btn.png", r"buttons\press-info-btn.png",
                       r"data\sounds\menu-button-sound.mp3")
-    exit_btn = Button(WIDTH // 2 - 240 // 2, 480, 240, 100, r"buttons\default-exit-btn.png",
+    exit_btn = Button(all_w + 323, all_h + 429, 240, 100, r"buttons\default-exit-btn.png",
                       r"buttons\hover-exit-btn.png", r"buttons\press-exit-btn.png",
                       r"data\sounds\menu-button-sound.mp3")
 
@@ -95,9 +97,13 @@ def main_menu():
                 if checkF11:
                     checkF11 = False
                     print('f11 off')
+                    change_fullScreen(1024, 704)
+                    main_menu()
                 else:
                     checkF11 = True
                     print('f11 on')
+                    change_fullScreen(1920, 1080, pygame.FULLSCREEN)
+                    main_menu()
 
             for button in [start_btn, settings_btn, info_btn, exit_btn]:
                 button.handle_event(event, volS)
@@ -120,39 +126,40 @@ def main_menu():
 
 def levels_menu():
     global checkF11
-
     if not checkF11:
-        all_w, all_h = WIDTH // 2 - 399, HEIGHT - 619
+        all_w, all_h = WIDTH // 2 - 395, HEIGHT - 565
     else:
-        all_w, all_h = WIDTH // 2 - 399, HEIGHT - 770
+        all_w, all_h = WIDTH // 2 - 395, HEIGHT - 770
+
     title = Object(all_w, all_h, 700, 82, r"objects\level-menu-title-obj.png")
+
     cross_btn = Button(all_w + title.width + 18, all_h + 8, 67, 72, r"buttons\default-cross-btn.png", r"buttons\hover-cross-btn.png",
                        r"buttons\press-cross-btn.png", r"data\sounds\menu-button-sound.mp3")
-    level1Button = Button(all_w - 49, all_h + 215.5, 144, 155, r"buttons\default-first-btn.png",
+    level1Button = Button(all_w - 58, all_h + 160, 144, 155, r"buttons\default-first-btn.png",
                           r"buttons\hover-first-btn.png", r"buttons\press-first-btn.png",
                           r"data\sounds\menu-button-sound.mp3")
-    level2Button = Button(all_w + 326, all_h + 215.5, 144, 155, r"buttons\default-second-btn.png",
+    level2Button = Button(all_w + 317, all_h + 160, 144, 155, r"buttons\default-second-btn.png",
                           r"buttons\hover-second-btn.png", r"buttons\press-second-btn.png",
                           r"data\sounds\menu-button-sound.mp3", r"buttons\no-active-second-btn.png")
-    levelBossButton = Button(all_w + 699, all_h + 215.5, 144, 155, r"buttons\default-boss-btn.png",
+    levelBossButton = Button(all_w + 692, all_h + 160, 144, 155, r"buttons\default-boss-btn.png",
                              r"buttons\hover-boss-btn.png", r"buttons\press-boss-btn.png",
                              r"data\sounds\menu-button-sound.mp3", r"buttons\no-active-boss-btn.png")
-    info_btn = Button(all_w + 857, all_h + 178, 18, 18, r"buttons\default-level-info-btn.png",
+    info_btn = Button(all_w + 858, all_h + 122.5, 18, 18, r"buttons\default-level-info-btn.png",
                       r"buttons\hover-level-info-btn.png")
 
-    field = Object(all_w - 91, all_h + 166, 980, 305, r"objects\level-menu-field-obj.png")
-    level1Field = Object(all_w - 70, all_h + 197.5, 186, 189, r"objects\start-level-field-obj.png")
-    level2Field = Object(all_w + 130, all_h + 197.5, 360, 189, r"objects\level-field-obj.png",
+    field = Object(all_w - 100, all_h + 111, 987, 305, r"objects\level-menu-field-obj.png")
+    level1Field = Object(all_w - 79, all_h + 142, 186, 189, r"objects\start-level-field-obj.png")
+    level2Field = Object(all_w + 121, all_h + 142, 360, 189, r"objects\level-field-obj.png",
                          r"objects\hover-level-field-obj.png")
-    levelBossField = Object(all_w + 503, all_h + 197.5, 360, 189,
+    levelBossField = Object(all_w + 496, all_h + 142, 360, 189,
                             r"objects\level-field-obj.png", r"objects\hover-level-field-obj.png")
 
     zeroStars, oneStar, twoStars, threeStars = (r"objects\stars-zero-obj.png", r"objects\stars-one-obj.png",
                                                 r"objects\stars-two-obj.png", r"objects\stars-three-obj.png")
-    level1Stars = Stars(all_w - 103, all_h + 395.5, 252, 44, zeroStars, oneStar, twoStars, threeStars)
-    level2Stars = Stars(all_w + 273, all_h + 395.5, 252, 44, zeroStars, oneStar, twoStars,
+    level1Stars = Stars(all_w - 113, all_h + 340, 252, 44, zeroStars, oneStar, twoStars, threeStars)
+    level2Stars = Stars(all_w + 262, all_h + 340, 252, 44, zeroStars, oneStar, twoStars,
                         threeStars)
-    levelBossStars = Stars(all_w + 647, all_h + 395.5, 252, 44, zeroStars, oneStar, twoStars,
+    levelBossStars = Stars(all_w + 639, all_h + 340, 252, 44, zeroStars, oneStar, twoStars,
                            threeStars)
 
     running = True
@@ -169,7 +176,7 @@ def levels_menu():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     transition()
-                    running = False
+                    main_menu()
 
             if event.type == pygame.USEREVENT and event.button == cross_btn:
                 transition()
@@ -190,12 +197,10 @@ def levels_menu():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
                 if checkF11:
                     checkF11 = False
-                    print('f11 off')
                     change_fullScreen(1024, 704)
                     levels_menu()
                 else:
                     checkF11 = True
-                    print('f11 on')
                     change_fullScreen(1920, 1080, pygame.FULLSCREEN)
                     levels_menu()
 
@@ -216,7 +221,7 @@ def levels_menu():
 
         info_btn.check_hover(pygame.mouse.get_pos())
         if info_btn.rect.collidepoint(pygame.mouse.get_pos()):
-            field_d = Object(all_w + 357, all_h - 40, 498, 218, r"objects\level-info-field-obj.png")
+            field_d = Object(all_w + 358.5, all_h - 95.5, 498, 218, r"objects\level-info-field-obj.png")
             field_d.draw(screen)
         info_btn.draw(screen)
 
@@ -242,30 +247,36 @@ def levels_menu():
 
 def settings_menu():
     global checkF11, isSliderMusic, isSliderSound, sl, sd, tmp, volS, wM, wS, checkActDet
-    title = Object(WIDTH // 2 - 626 // 2 - 50, 85, 626, 82, r"objects\settings-title-obj.png")
-    field_audio = Object(WIDTH // 2 - 450, 200, 420, 430, r"objects\audio-field-obj.png")
-    field_video = Object(WIDTH // 2 + 30, 200, 420, 430, r"objects\video-field-obj.png")
-    fs_name = Object(WIDTH // 2 + 478 // 2 - 332 // 2, 330, 332, 75, r"objects\fullscreen-obj.png")
-    sound_name = Object(WIDTH // 2 - 450 // 2 - 332 // 2 - 18, 470, 332, 35, r"objects\sound-obj.png")
-    music_name = Object(WIDTH // 2 - 450 // 2 - 332 // 2 - 18, 334, 332, 35, r"objects\music-obj.png")
+    if not checkF11:
+        all_w, all_h = WIDTH // 2 - 363, HEIGHT - 619
+    else:
+        all_w, all_h = WIDTH // 2 - 363, HEIGHT - 820
 
-    cross_btn = Button(WIDTH - 229, 93, 67, 72, r"buttons\default-cross-btn.png", r"buttons\hover-cross-btn.png",
+    title = Object(all_w, all_h, 626, 82, r"objects\settings-title-obj.png")
+
+    field_audio = Object(all_w - 87, all_h + 115, 420, 430, r"objects\audio-field-obj.png")
+    field_video = Object(all_w + 393, all_h + 115, 420, 430, r"objects\video-field-obj.png")
+    fs_name = Object(all_w + 436, all_h + 245, 332, 75, r"objects\fullscreen-obj.png")
+    sound_name = Object(all_w - 46, all_h + 385, 332, 35, r"objects\sound-obj.png")
+    music_name = Object(all_w - 46, all_h + 249, 332, 35, r"objects\music-obj.png")
+
+    cross_btn = Button(all_w + 646, all_h + 8, 67, 72, r"buttons\default-cross-btn.png", r"buttons\hover-cross-btn.png",
                        r"buttons\press-cross-btn.png", r"data\sounds\menu-button-sound.mp3")
-    fs_btn = Button(WIDTH // 2 + 478 // 2 - 136 // 2, 420, 136, 62, r"buttons\fullscreen-off-btn.png", "",
+    fs_btn = Button(all_w + 534, all_h + 335, 136, 62, r"buttons\fullscreen-off-btn.png", "",
                     r"buttons\fullscreen-on-btn.png", r"data\sounds\menu-button-sound.mp3")
 
     sl = 106 + 300 * wM
-    music_slider_btn = Button(sl, 387, 26, 28,
+    music_slider_btn = Button(sl, all_h + 302, 26, 28,
                               r"buttons\default-slider-btn.png", r"buttons\hover-slider-btn.png",
                               r"buttons\press-slider-btn.png")
 
     sd = 106 + 300 * wS
-    sound_slider_btn = Button(sd, 523, 26, 28,
+    sound_slider_btn = Button(sd, all_h + 438, 26, 28,
                               r"buttons\default-slider-btn.png", r"buttons\hover-slider-btn.png",
                               r"buttons\press-slider-btn.png")
 
-    music_slider_obj = Object(WIDTH // 2 - 450 // 2 - 302 // 2 - 18, 393, 302, 16, r"objects\slider-obj.png")
-    sound_slider_obj = Object(WIDTH // 2 - 450 // 2 - 302 // 2 - 18, 529, 302, 16, r"objects\slider-obj.png")
+    music_slider_obj = Object(all_w - 31, all_h + 308, 302, 16, r"objects\slider-obj.png")
+    sound_slider_obj = Object(all_w - 31, all_h + 444, 302, 16, r"objects\slider-obj.png")
     running = True
     while running:
         clock.tick(fps)
@@ -279,22 +290,22 @@ def settings_menu():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     transition()
-                    running = False
+                    main_menu()
 
             if event.type == pygame.USEREVENT and event.button == cross_btn:
                 transition()
-                running = False
+                main_menu()
 
             if ((event.type == pygame.USEREVENT and event.button == fs_btn) or
                     (event.type == pygame.KEYDOWN and event.key == pygame.K_F11)):
                 if checkF11:
                     checkF11 = False
-                    print('f11 off')
                     change_fullScreen(1024, 704)
+                    settings_menu()
                 else:
                     checkF11 = True
-                    print('f11 on')
                     change_fullScreen(1920, 1080, pygame.FULLSCREEN)
+                    settings_menu()
 
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == music_slider_btn:
                 isSliderMusic = True
@@ -307,7 +318,7 @@ def settings_menu():
                     checkActDet = open(r"data\settings.txt", "w")
                     if isSliderMusic:
                         xM = music_slider_btn.rect[0]
-                        if 118 < event.pos[0] < 420:
+                        if all_w - 32 < event.pos[0] < all_w + 270:
                             x_cube_M = event.pos[0] - xM
                         else:
                             x_cube_M = 13
@@ -318,7 +329,7 @@ def settings_menu():
 
                     elif isSliderSound:
                         xS = sound_slider_btn.rect[0]
-                        if 118 < event.pos[0] < 420:
+                        if all_w - 32 < event.pos[0] < all_w + 270:
                             x_cube_S = event.pos[0] - xS
                         else:
                             x_cube_S = 13
@@ -363,19 +374,26 @@ def settings_menu():
 
 def info_menu():
     global checkF11
-    cross_btn = Button(WIDTH - 218, 93, 67, 72, r"buttons\default-cross-btn.png", r"buttons\hover-cross-btn.png",
-                       r"buttons\press-cross-btn.png", r"data\sounds\menu-button-sound.mp3")
-    github_left_btn = Button(WIDTH // 2 - 345, HEIGHT - 170, 67, 72, r"buttons\default-github-btn.png",
+    if not checkF11:
+        all_w, all_h = WIDTH // 2 - 364, HEIGHT - 619
+    else:
+        all_w, all_h = WIDTH // 2 - 364, HEIGHT - 820
+
+    title = Object(all_w, all_h, 640, 82, r"objects\info-title-obj.png")
+
+    cross_btn = Button(all_w + 660, all_h + 8, 67, 72, r"buttons\default-cross-btn.png",
+                       r"buttons\hover-cross-btn.png", r"buttons\press-cross-btn.png",
+                       r"data\sounds\menu-button-sound.mp3")
+    github_left_btn = Button(all_w + 19, all_h + 449, 67, 72, r"buttons\default-github-btn.png",
                              r"buttons\hover-github-btn.png", r"buttons\press-github-btn.png",
                              r"data\sounds\menu-button-sound.mp3")
-    github_right_btn = Button(WIDTH // 2 + 100, HEIGHT - 170, 67, 72, r"buttons\default-github-btn.png",
+    github_right_btn = Button(all_w + 464, all_h + 449, 67, 72, r"buttons\default-github-btn.png",
                               r"buttons\hover-github-btn.png", r"buttons\press-github-btn.png",
                               r"data\sounds\menu-button-sound.mp3")
 
-    title = Object(WIDTH // 2 - 640 // 2 - 46, 85, 640, 82, r"objects\info-title-obj.png")
-    field = Object(WIDTH // 2 - 450, 200, 900, 430, r"objects\info-field-obj.png")
-    alexandr = Object(WIDTH // 2 - 265, HEIGHT - 157, 269, 46, r"objects\alexandr-obj.png")
-    igor = Object(WIDTH // 2 + 180, HEIGHT - 157, 142, 45, r"objects\igor-obj.png")
+    field = Object(all_w - 86, all_h + 115, 900, 430, r"objects\info-field-obj.png")
+    alexandr = Object(all_w + 99, all_h + 462, 269, 46, r"objects\alexandr-obj.png")
+    igor = Object(all_w + 544, all_h + 462, 142, 45, r"objects\igor-obj.png")
 
     running = True
     while running:
@@ -391,11 +409,11 @@ def info_menu():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     transition()
-                    running = False
+                    main_menu()
 
             if event.type == pygame.USEREVENT and event.button == cross_btn:
                 transition()
-                running = False
+                main_menu()
 
             if event.type == pygame.USEREVENT and event.button == github_left_btn:
                 webbrowser.open('https://github.com/mikhalexandr')
@@ -406,10 +424,12 @@ def info_menu():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
                 if checkF11:
                     checkF11 = False
-                    print('f11 off')
+                    change_fullScreen(1024, 704)
+                    info_menu()
                 else:
                     checkF11 = True
-                    print('f11 on')
+                    change_fullScreen(1920, 1080, pygame.FULLSCREEN)
+                    info_menu()
 
             for button in [github_left_btn, github_right_btn, cross_btn]:
                 button.handle_event(event, volS)
@@ -431,15 +451,38 @@ def info_menu():
         pygame.display.flip()
 
 
+def transition():
+    running = True
+    transition_level = 0
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        transition_surface = pygame.Surface((WIDTH, HEIGHT))
+        transition_surface.fill((0, 0, 0))
+        transition_surface.set_alpha(transition_level)
+        screen.blit(transition_surface, (0, 0))
+
+        transition_level += 5
+        if transition_level >= 105:
+            transition_level = 255
+            running = False
+
+        pygame.display.flip()
+        clock.tick(70)
+
+
 def change_fullScreen(width, height, fullScreen=1):
     global WIDTH, HEIGHT, screen, img, bg
     WIDTH, HEIGHT = width, height
     screen = pygame.display.set_mode((WIDTH, HEIGHT), fullScreen)
     if checkF11:
-        tmp = load_image(r"backgrounds\main-menu-fullScreen-bg.png")
-        bg = pygame.transform.scale(tmp, (tmp.get_width(), tmp.get_height()))
+        tmp_img = load_image(r"backgrounds\main-menu-fullScreen-bg.png")
+        bg = pygame.transform.scale(tmp_img, (tmp_img.get_width(), tmp_img.get_height()))
     else:
         bg = pygame.transform.scale(img, (img.get_width() * 2, img.get_height() * 2))
 
 
-# main_menu()
+main_menu()
